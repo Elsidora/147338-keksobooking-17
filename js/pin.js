d572ef5ac259fd7ac06624955d5f087c0267f62a
elsidora <ella-sidorova@mail.ru>
2019-06-15 23:27:49 +0300

    module4-task1, start

---
+++ js/pin.js
@@ -0,0 +1,45 @@
+'use strict';
+(function () {
+  var AD_AMOUNT = 8;
+  var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+  var main = document.querySelector('main');
+  var adForm = main.querySelector('.ad-form');
+  var mapBlock = main.querySelector('.map');
+  var pin = mapBlock.querySelector('.map__pin--main');
+  var fieldsetForm = main.querySelectorAll('fieldset');
+  var selectForm = main.querySelectorAll('select');
+
+  var pinBox = mapBlock.querySelector('.map__pins');
+
+  var pinSize = {
+    width: 50,
+    height: 70
+  };
+
+  function createPin(adObject) {
+    var pinElement = pinTemplate.cloneNode(true);
+    var image = pinElement.querySelector('img');
+    pinElement.style.left = adObject.location.x - pinSize.width / 2 + 'px';
+    pinElement.style.top = adObject.location.y + pinSize.height + 'px';
+    image.src = adObject.author.avatar;
+    image.alt = adObject.offer.title;
+    return pinElement;
+  }
+
+  function onPinClick(evt) {
+    evt.preventDefault();
+    mapBlock.classList.remove('map--faded');
+    adForm.classList.remove('ad-form--disabled');
+    window.util.addAttribute(mapBlock, 'map--faded', fieldsetForm, 'disabled');
+    window.util.addAttribute(mapBlock, 'map--faded', selectForm, 'disabled');
+    var arrObjects = window.data.getAdsObjects(AD_AMOUNT, pinBox);
+    window.util.renderElements(arrObjects, pinBox, window.pin.createPin);
+
+  }
+
+  pin.addEventListener('click', onPinClick);
+
+  window.pin = {
+    createPin: createPin
+  };
+})();
